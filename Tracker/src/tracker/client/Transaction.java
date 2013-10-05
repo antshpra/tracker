ab9d7c03e93fc2d5288287136013abe031b5ebe5
Prashant <prashant@claymus.com>
2013-10-05 14:29:40 +0530

    Adding transactions page.

---
+++ Tracker/src/tracker/client/Transaction.java
@@ -0,0 +1,208 @@
+package tracker.client;
+
+import java.util.Date;
+import java.util.List;
+
+import tracker.shared.CreateTransactionRequest;
+import tracker.shared.GetTransactionsResponse;
+import tracker.shared.utils.DateUtil;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.datepicker.client.DatePicker;
+
+public class Transaction implements EntryPoint {
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+
+	private final TransactionServiceAsync transactionService = GWT.create(TransactionService.class);
+
+	private FlexTable transactionsTable = new FlexTable();
+	private Panel transactionsSubPanel = new SimplePanel();
+	
+	private Date lastTransactionDate = new Date();
+	private int transactionsPerRequestCount = 10;
+
+	private Label loadingTransactionsLabel = new Label( "Loading Transactions ..." );
+	private Label noMoreTransactionsLabel = new Label( "No More Transactions !" );
+	private Anchor loadMoreTransactionsAnchor = new Anchor( "Load More Transactions" ); 
+	
+	
+	public void onModuleLoad() {
+		
+		final DatePicker datePicker = new DatePicker();
+		
+		ListBox yearList = new ListBox();
+		for( int i = 2000; i <= 2025; i++ )
+			yearList.addItem( Integer.toString( i ) );
+
+		ListBox monthList = new ListBox();
+		for( int i = 0; i <= 12; i++ )
+			monthList.addItem( Integer.toString( i ) );
+		
+		
+		
+		final TextBox descriptionInput = new TextBox();
+		Button saveButton = new Button( "Save" );
+		
+		saveButton.addClickHandler( new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				CreateTransactionRequest request = new CreateTransactionRequest();
+				
+				try {
+					request.setDescription( descriptionInput.getText() );
+					request.setTransactionDate( datePicker.getValue() );
+				} catch( IllegalArgumentException ex ) {
+					Window.alert( ex.getMessage() );
+				}
+				
+				transactionService.createTransaction( request, new AsyncCallback<Long>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void onSuccess(Long result) {
+						Window.alert( result + "" );
+					}
+					
+				});
+				
+			}
+		});
+		
+		RootPanel.get().add( datePicker );		
+		RootPanel.get().add( descriptionInput );
+		RootPanel.get().add( saveButton );
+		
+		
+		
+		loadMoreTransactionsAnchor.addClickHandler( new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				loadTransactions( Transaction.this.lastTransactionDate, Transaction.this.transactionsPerRequestCount );				
+			}
+			
+		});
+		
+		transactionsTable.addStyleName( "table-style-1" );
+		
+		RootPanel.get().add( this.transactionsTable );
+		RootPanel.get().add( this.transactionsSubPanel );
+
+		loadTransactions( new Date(), 15 );
+		
+	}
+	
+	private void loadTransactions( final Date olderThan, final int count ) {
+		
+		this.transactionsSubPanel.clear();
+		this.transactionsSubPanel.add( this.loadingTransactionsLabel );
+		
+		transactionService.getTransactions( olderThan, count,
+				
+				new AsyncCallback<List<GetTransactionsResponse>>() {
+			
+					public void onFailure( Throwable caught ) {
+						// Show the RPC error message to the user
+//						dialogBox
+//								.setText("Remote Procedure Call - Failure");
+//						serverResponseLabel
+//								.addStyleName("serverResponseLabelError");
+//						serverResponseLabel.setHTML(SERVER_ERROR);
+//						dialogBox.center();
+//						closeButton.setFocus(true);
+//						
+//						ERROR MESSAGE + RETRY MESAGE
+					}
+					
+					public void onSuccess( List<GetTransactionsResponse> transactionDetailList ) {
+						
+						for( GetTransactionsResponse transactionDetail : transactionDetailList ) {
+							int rowNum = Transaction.this.transactionsTable.getRowCount();
+							
+							final HTML dateTimeHTML = new HTML( DateUtil.getDay( transactionDetail.getCreationDate() ) + "<br/>" + DateUtil.getTime( transactionDetail.getCreationDate() ) );
+							final HTML descriptionHTML = new HTML( transactionDetail.getDescription() + "<br/>added by " + transactionDetail.getCreatedBy() );
+							
+							MouseOverHandler mouseOverHandler = new MouseOverHandler() {
+								@Override
+								public void onMouseOver( MouseOverEvent event ) {
+									dateTimeHTML.addStyleName( "highlighted" );
+									descriptionHTML.addStyleName( "highlighted" );
+								}
+							};
+
+							MouseOutHandler mouseOutHandler = new MouseOutHandler() {
+								@Override
+								public void onMouseOut( MouseOutEvent event ) {
+									dateTimeHTML.removeStyleName( "highlighted" );
+									descriptionHTML.removeStyleName( "highlighted" );
+								}
+							};
+							
+							final long transId = transactionDetail.getId();
+							MouseUpHandler mouseUpHandler = new MouseUpHandler() {
+								@Override
+								public void onMouseUp( MouseUpEvent event ) {
+									Window.alert( transId + "" );
+								}
+							};
+							
+							dateTimeHTML.addMouseOverHandler( mouseOverHandler );
+							descriptionHTML.addMouseOverHandler( mouseOverHandler );
+							
+							dateTimeHTML.addMouseOutHandler( mouseOutHandler );
+							descriptionHTML.addMouseOutHandler( mouseOutHandler );
+							
+							dateTimeHTML.addMouseUpHandler( mouseUpHandler );
+							descriptionHTML.addMouseUpHandler( mouseUpHandler );
+							
+							Transaction.this.transactionsTable.setWidget( rowNum, 0, dateTimeHTML );
+							Transaction.this.transactionsTable.setWidget( rowNum, 1, descriptionHTML );
+						}
+						
+						if( transactionDetailList.size() != 0 ) {
+							Transaction.this.lastTransactionDate = transactionDetailList.get( transactionDetailList.size() - 1 ).getCreationDate();
+						}
+						
+						if( transactionDetailList.size() < count ) {
+							Transaction.this.transactionsSubPanel.clear();
+							Transaction.this.transactionsSubPanel.add( Transaction.this.noMoreTransactionsLabel );
+						} else {
+							Transaction.this.transactionsSubPanel.clear();
+							Transaction.this.transactionsSubPanel.add( Transaction.this.loadMoreTransactionsAnchor );
+						}
+					}
+					
+				});
+
+	}
+	
+}
---
+++ Tracker/src/tracker/shared/utils/DateUtil.java
@@ -0,0 +1,97 @@
+package tracker.shared.utils;
+
+import java.util.Date;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+public class DateUtil {
+
+	private static final long MILLISECONDS_IN_A_SECOND = 1000;
+	private static final long MILLISECONDS_IN_A_MINUTE = 60 * MILLISECONDS_IN_A_SECOND;
+	private static final long MILLISECONDS_IN_A_HOUR   = 60 * MILLISECONDS_IN_A_MINUTE;
+	private static final long MILLISECONDS_IN_A_DAY	   = 24 * MILLISECONDS_IN_A_HOUR;
+	
+	private static final long TZ_IST_MILLISECONDS = (long) (5.5 * 60 * 60 * 1000); 
+	
+	private static final String DAY_COMING = "Coming ";
+	
+	private static final String DAY_TOMORROW  = "Tomorrow";
+	private static final String DAY_TODAY 	  = "Today";
+	private static final String DAY_YESTERDAY = "Yesterday";
+	
+	private static final String TIME_FEW_MOMENTS_AGO	   = "Few Moments Ago";
+	private static final String TIME_FEW_MINUTES_AGO	   = "Few Minutes Ago";
+	private static final String TIME_lESS_THAN_AN_HOUR_AGO = "Less Than an Hour Ago";
+	private static final String TIME_ABOUT_AN_HOUR_AGO	   = "Abour an Hour Ago";
+	private static final String TIME_FEW_HOURS_AGO		   = "Few Hours Ago";
+	
+	private DateUtil() {}
+
+	
+	public static String getDay( Date date ) {
+		
+		long date_tz_day = ( date.getTime() + TZ_IST_MILLISECONDS ) / MILLISECONDS_IN_A_DAY;
+		long today_tz_day = ( new Date().getTime() + TZ_IST_MILLISECONDS ) / MILLISECONDS_IN_A_DAY;
+		
+		DateTimeFormat dateTimeFormat;
+		
+		if( date_tz_day - today_tz_day >= 31 )
+			return DateTimeFormat.getFormat( "d MMM yyyy" ).format( date );
+		
+		else if( date_tz_day - today_tz_day >= 5 )
+			return DateTimeFormat.getFormat( "d MMMMMM" ).format( date );
+		
+		else if( date_tz_day - today_tz_day >= 2 )
+			return DAY_COMING + DateTimeFormat.getFormat( "EEEE" ).format( date );
+		
+		else if( date_tz_day - today_tz_day == 1 )
+			return DAY_TOMORROW;
+			
+		else if( date_tz_day - today_tz_day == 0 )
+			return DAY_TODAY;
+			
+		else if( date_tz_day - today_tz_day == -1 )
+			return DAY_YESTERDAY;
+			
+		else if( date_tz_day - today_tz_day >= -4 )
+			return DateTimeFormat.getFormat( "EEEE" ).format( date );
+			
+		else if( date_tz_day - today_tz_day >= -30 )
+			return DateTimeFormat.getFormat( "d MMMMMM" ).format( date );
+			
+		else
+			return DateTimeFormat.getFormat( "d MMM yyyy" ).format( date );
+		
+	}
+	
+	public static String getTime( Date date ) {
+		
+		long date_tz_min = date.getTime() / MILLISECONDS_IN_A_MINUTE;
+		long now_tz_min = new Date().getTime() / MILLISECONDS_IN_A_MINUTE;
+		
+		DateTimeFormat dateTimeFormat;
+		
+		if( date_tz_min - now_tz_min > 0 )
+			return DateTimeFormat.getFormat( "h:mm a" ).format( date );
+		
+		else if( date_tz_min - now_tz_min >= -2 )
+			return TIME_FEW_MOMENTS_AGO;
+			
+		else if( date_tz_min - now_tz_min >= -15 )
+			return TIME_FEW_MINUTES_AGO;
+		
+		else if( date_tz_min - now_tz_min >= -45 )
+			return TIME_lESS_THAN_AN_HOUR_AGO;
+		
+		else if( date_tz_min - now_tz_min >= -75 )
+			return TIME_ABOUT_AN_HOUR_AGO;
+		
+		else if( date_tz_min - now_tz_min >= -240 )
+			return TIME_FEW_HOURS_AGO;
+				
+		else
+			return DateTimeFormat.getFormat( "h:mm a" ).format( date );
+		
+	}
+	
+}
