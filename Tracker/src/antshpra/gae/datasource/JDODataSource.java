d69a4cebf1fd373739e0cccc3ae1251c30d55e33
Prashant <prashant@claymus.com>
2013-10-09 01:21:33 +0530

    Renaming GAEDataSource to JDODataSource.

--- Tracker/src/antshpra/gae/datasource/JDODataSource.java
+++ Tracker/src/antshpra/gae/datasource/JDODataSource.java
@@ -4,15 +4,15 @@ import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 
-public class GAEDataSource {
+public class JDODataSource {
 
 	private static final PersistenceManagerFactory pmfInstance =
 			JDOHelper.getPersistenceManagerFactory( "transactions-optional" );
 
 	protected final PersistenceManager pm;
 	
-	protected GAEDataSource() {
-		this.pm = GAEDataSource.pmfInstance.getPersistenceManager();
+	protected JDODataSource() {
+		this.pm = JDODataSource.pmfInstance.getPersistenceManager();
 	}
 
 	public final void close() {
