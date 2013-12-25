871281bc5ba2d5622200d8607b4937180c8b1fe4
Prashant <prashant@claymus.com>
2013-12-25 08:43:28 +0530

    Updated HomePage page to use updated widgets.

--- Tracker/src/tracker/page/home/client/HomePage.java
+++ Tracker/src/tracker/page/home/client/HomePage.java
@@ -1,6 +1,8 @@
 package tracker.page.home.client;
 
+import tracker.module.transaction.client.EditTransaction;
 import tracker.module.transaction.client.TransactionList;
+import tracker.module.transaction.client.TransactionListLoader;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -9,7 +11,8 @@ public class HomePage implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().add( new TransactionList().getWidget() );
+		RootPanel.get().add( new EditTransaction() );
+		RootPanel.get().add( new TransactionListLoader( new TransactionList() ) );
 	}
 
 }
