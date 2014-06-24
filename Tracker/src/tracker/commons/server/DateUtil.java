f5350a264ea1a70bc2c417bbc77cc12715c68ab3
Prashant <prashant@claymus.com>
2014-06-24 16:14:49 +0530

    Added UnitTest for DateUtil.

--- Tracker/src/tracker/commons/server/DateUtil.java
+++ Tracker/src/tracker/commons/server/DateUtil.java
@@ -2,19 +2,27 @@ package tracker.commons.server;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateUtil {
 
 	public static Date getDateFor( int year, int month, int day ) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.set( year, month, day );
+		Calendar calendar = Calendar.getInstance( TimeZone.getTimeZone( "IST" ) );
+		calendar.setTimeInMillis( 0 );
+		calendar.set( year, month, day, 0, 0, 0 );
 		return calendar.getTime();
 	}
 	
 	public static int getYear( Date date ) {
-		Calendar calendar = Calendar.getInstance();
+		Calendar calendar = Calendar.getInstance( TimeZone.getTimeZone( "IST" ) );
 		calendar.setTime( date );
 		return calendar.get( Calendar.YEAR );
+	}
+	
+	public static int getMonth( Date date ) {
+		Calendar calendar = Calendar.getInstance( TimeZone.getTimeZone( "IST" ) );
+		calendar.setTime( date );
+		return calendar.get( Calendar.MONTH );
 	}
 	
 }
---
+++ Tracker/test/tracker/commons/server/DateUtilTest.java
@@ -0,0 +1,70 @@
+package tracker.commons.server;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilTest {
+
+	@Test
+	public void getDateForTest_1() {
+		DateFormat dateFormat = new SimpleDateFormat( "dd-MM-yyyy HH:mm:ss z" );
+
+		Date date = DateUtil.getDateFor( 2014, 00, 01 );
+		String dateStr = dateFormat.format( date );
+		Assert.assertEquals( "01-01-2014 00:00:00 IST", dateStr );
+		
+		date = DateUtil.getDateFor( 2014, 11, 31 );
+		dateStr = dateFormat.format( date );
+		Assert.assertEquals( "31-12-2014 00:00:00 IST", dateStr );
+
+		date = DateUtil.getDateFor( 2014, 12, 01 );
+		dateStr = dateFormat.format( date );
+		Assert.assertEquals( "01-01-2015 00:00:00 IST", dateStr );
+	}
+	
+	@Test
+	public void getDateForTest_2() throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat( "dd-MM-yyyy HH:mm:ss z" );
+
+		Date date = DateUtil.getDateFor( 2014, 00, 01 );
+		Assert.assertEquals( formatter.parse( "01-01-2014 00:00:00 IST" ).getTime(), date.getTime() );
+		
+		date = DateUtil.getDateFor( 2014, 11, 31 );
+		Assert.assertEquals( formatter.parse( "31-12-2014 00:00:00 IST" ).getTime(), date.getTime() );
+
+		date = DateUtil.getDateFor( 2014, 12, 01 );
+		Assert.assertEquals( formatter.parse( "01-01-2015 00:00:00 IST" ).getTime(), date.getTime() );
+	}
+	
+	@Test
+	public void getYearTest() throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat( "dd-MM-yyyy HH:mm:ss z" );
+		
+		Date date = formatter.parse( "01-01-2014 00:00:00 IST" );
+		int year = DateUtil.getYear( date );
+		Assert.assertEquals( 2014, year );
+		
+		date = formatter.parse( "31-12-2014 23:59:59 IST" );
+		year = DateUtil.getYear( date );
+		Assert.assertEquals( 2014, year );
+	}
+	
+	@Test
+	public void getMonthTest() throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat( "dd-MM-yyyy HH:mm:ss z" );
+		
+		Date date = formatter.parse( "01-01-2014 00:00:00 IST" );
+		int month = DateUtil.getMonth( date );
+		Assert.assertEquals( 0, month );
+		
+		date = formatter.parse( "31-12-2014 23:59:59 IST" );
+		month = DateUtil.getMonth( date );
+		Assert.assertEquals( 11, month );
+	}
+	
+}
